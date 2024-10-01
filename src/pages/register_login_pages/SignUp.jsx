4347a3c6b77ab33287d0a99be26a6396ff19a63d
kyrolis <kerodex224@gmail.com>
2024-10-01 17:05:52 +0300

    register and login second update fix the login and register issue and add error message

--- src/pages/register_login_pages/SignUp.jsx
+++ src/pages/register_login_pages/SignUp.jsx
@@ -10,204 +10,215 @@ const SignUp = () => {
 		lastName: '',
 		email: '',
 		password: '',
-		phone: '',
+		phonenumber: '',
 		confirmPassword: '',
 		gender: '',
 	})
-	const [firstName, serFirstName] = useState(false)
-	const [lastName, setLastName] = useState(false)
-	const [email, setEmail] = useState(false)
-	const [phone, setPhone] = useState(false)
-	const [password, setPassword] = useState(false)
-	const [ConfiremPassword, setConfiremPassword] = useState(false)
-	const [gender, setGender] = useState(false)
+
+	const [errors, setErrors] = useState({})
+	const [showPassword, setShowPassword] = useState(false)
 	const navigate = useNavigate()
 
-	const handleOnSumbit = (e) => {
+	const updateUserInput = (e) => {
+		const { name, value } = e.target
+		setUser((prevUser) => ({
+			...prevUser,
+			[name]: value,
+		}))
+	}
+
+	const handleOnSubmit = (e) => {
 		e.preventDefault()
-		if (user.firstName == '') {
-			serFirstName(true)
-		} else if (user.lastName == '') {
-			setLastName(true)
-		} else if (user.email == '') {
-			setEmail(false)
-		} else if (user.phone == '') {
-			setPhone(true)
-		} else if (user.password == '') {
-			setPassword(true)
-		} else if (user.confirmPassword == '') {
-			setConfiremPassword(true)
-		} else if (user.gender == '') {
-			setGender(true)
+
+		const newErrors = {}
+
+		Object.keys(user).forEach((key) => {
+			if (user[key] === '') {
+				newErrors[key] = `${
+					key.charAt(0).toUpperCase() + key.slice(1)
+				} is required`
+			}
+		})
+
+		if (user.password !== user.confirmPassword) {
+			newErrors.confirmPassword = "Passwords don't match"
 		}
+
+		if (Object.keys(newErrors).length > 0) {
+			setErrors(newErrors)
+			return
+		}
+
 		const userInfo = {
 			firstName: user.firstName,
 			lastName: user.lastName,
 			email: user.email,
 			password: user.password,
-			phone: user.phone,
+			phonenumber: user.phonenumber,
 			gender: user.gender,
 			cart: [],
 		}
-		axios({
-			method: 'post',
-			url: 'https://booming-odd-lark.glitch.me/users',
-			data: userInfo,
-		})
-			.then((e) => {
-				//your state value
+
+		axios
+			.post('https://booming-odd-lark.glitch.me/users', userInfo)
+			.then(() => {
 				navigate('/login')
 			})
-			.catch((e) => console.log(e.message))
+			.catch((error) => console.error(error.message))
 	}
 
 	return (
-		<div>
-			<div className='flex w-full h-screen'>
-				<div className='hidden lg:flex h-full item-center justify-center'>
-					<img
-						src={img1}
-						alt='the Sign_up image'
-					/>
-				</div>
-				<div className='w-full lg:w-2/3 flex items-center justify-center bg-gray-100'>
-					<form
-						onSubmit={(e) => {
-							handleOnSumbit(e)
-						}}
-						className=' bg-white shadow-xl px-10 py-10 rounded-3xl border-2 '
-					>
-						<h1 className=' flex  justify-center text-3xl  font-bold mb-10 font-body  '>
-							Create New Account
-						</h1>
+		<div className='flex w-full h-screen'>
+			<div className='hidden lg:flex h-full item-center justify-center'>
+				<img
+					src={img1}
+					alt='the Sign_up image'
+				/>
+			</div>
+			<div className='w-full lg:w-2/3 flex items-center justify-center bg-gray-100'>
+				<form
+					onSubmit={handleOnSubmit}
+					className='bg-white shadow-xl px-10 py-10 rounded-3xl border-2'
+				>
+					<h1 className='flex justify-center text-3xl font-bold mb-10'>
+						Create New Account
+					</h1>
 
-						<div className='grid grid-cols-1 lg:grid-cols-2 justify-center w-full  gap-6 lg:gap-9'>
+					<div className='grid grid-cols-1 lg:grid-cols-2 justify-center w-full gap-6 lg:gap-9'>
+						<div>
+							{errors.firstName && (
+								<p className='text-red-500 font-bold mb-1 font-bold mb-1'>
+									{errors.firstName}
+								</p>
+							)}
 							<Input
+								name='firstName'
 								color='blue'
-								label='FirstName'
-								error={firstName}
+								label='First Name'
+								error={Boolean(errors.firstName)}
 								value={user.firstName}
-								onChange={(e) =>
-									setUser({
-										...user,
-										firstName: e.target.value,
-									})
-								}
+								onChange={updateUserInput}
 							/>
+						</div>
+						<div>
+							{errors.lastName && (
+								<p className='text-red-500 font-bold mb-1 '>
+									{errors.lastName}
+								</p>
+							)}
 							<Input
-								error={lastName}
+								name='lastName'
 								color='blue'
-								label='LastName'
+								label='Last Name'
+								error={Boolean(errors.lastName)}
 								value={user.lastName}
-								onChange={(e) =>
-									setUser({
-										...user,
-										lastName: e.target.value,
-									})
-								}
+								onChange={updateUserInput}
 							/>
+						</div>
+						<div>
+							{errors.email && (
+								<p className='text-red-500 font-bold mb-1'>{errors.email}</p>
+							)}
 							<Input
-								error={email}
+								name='email'
 								color='blue'
 								label='Email'
+								error={Boolean(errors.email)}
 								value={user.email}
-								onChange={(e) =>
-									setUser({
-										...user,
-										email: e.target.value,
-									})
-								}
+								onChange={updateUserInput}
 							/>
+						</div>
+						<div>
+							{errors.phonenumber && (
+								<p className='text-red-500 font-bold mb-1'>
+									{errors.phonenumber}
+								</p>
+							)}
 							<Input
+								name='phonenumber'
 								color='blue'
-								label='Phone'
-								error={phone}
-								value={user.phone}
-								onChange={(e) =>
-									setUser({
-										...user,
-										phone: e.target.value,
-									})
-								}
+								label='phonenumber'
+								error={Boolean(errors.phonenumber)}
+								value={user.phonenumber}
+								onChange={updateUserInput}
 							/>
+						</div>
+						<div>
+							{errors.password && (
+								<p className='text-red-500 font-bold mb-1'>{errors.password}</p>
+							)}
 							<Input
+								name='password'
 								color='blue'
-								type='password'
-								label='password'
-								error={password}
+								type={showPassword ? 'text' : 'password'}
+								label='Password'
+								error={Boolean(errors.password)}
 								value={user.password}
-								onChange={(e) =>
-									setUser({
-										...user,
-										password: e.target.value,
-									})
-								}
+								onChange={updateUserInput}
 							/>
+						</div>
+						<div>
+							{errors.confirmPassword && (
+								<p className='text-red-500 font-bold mb-1'>
+									{errors.confirmPassword}
+								</p>
+							)}
 							<Input
+								name='confirmPassword'
 								color='blue'
-								type='password'
-								label='Confirem password'
-								error={ConfiremPassword}
+								type={showPassword ? 'text' : 'password'}
+								label='Confirm Password'
+								error={Boolean(errors.confirmPassword)}
 								value={user.confirmPassword}
-								onChange={(e) =>
-									setUser({
-										...user,
-										confirmPassword: e.target.value,
-									})
-								}
+								onChange={updateUserInput}
 							/>
 						</div>
-						<Checkbox
-							label='show password'
-							color='blue'
-						/>
-						<div className='flex m-4 '>
-							<p className='text-xl m-2  '>
-								{gender == true ? 'Please select one' : 'Gender'}
-							</p>
-							<Radio
-								name='type'
-								label='Male'
-								value='male'
-								color='blue'
-								onChange={(e) =>
-									setUser({
-										...user,
-										gender: e.target.value,
-									})
-								}
-							/>
-							<Radio
-								color='blue'
-								name='type'
-								value='female'
-								label='Female'
-								onChange={(e) =>
-									setUser({
-										...user,
-										gender: e.target.value,
-									})
-								}
-							/>
-						</div>
-						<Button
-							className=' normal-case items-center text-lg  text-center w-full '
-							color='blue'
-							type='submit'
-						>
-							Create Account
-						</Button>
-						<p className='flex items-center justify-center mt-5'>
-							Already have an account?
-							<Link
-								className='text-blue-800 pl-1 hover:font-bold hover:-translate-y-0.5'
-								to='/login'
-							>
-								Log in
-							</Link>{' '}
+					</div>
+
+					<Checkbox
+						label='Show Password'
+						color='blue'
+						onChange={() => setShowPassword((prev) => !prev)}
+					/>
+
+					<div className='flex m-4'>
+						<p className='text-xl m-2'>
+							{user.gender === '' ? 'Please select one' : 'Gender'}
 						</p>
-					</form>
-				</div>
+						<Radio
+							name='gender'
+							label='Male'
+							value='male'
+							color='blue'
+							onChange={updateUserInput}
+						/>
+						<Radio
+							name='gender'
+							value='female'
+							label='Female'
+							color='blue'
+							onChange={updateUserInput}
+						/>
+					</div>
+
+					<Button
+						className='normal-case items-center text-lg text-center w-full'
+						color='blue'
+						type='submit'
+					>
+						Create Account
+					</Button>
+
+					<p className='flex items-center justify-center mt-5'>
+						Already have an account?
+						<Link
+							className='text-blue-800 pl-1 hover:font-bold hover:-translate-y-0.5'
+							to='/login'
+						>
+							Log in
+						</Link>
+					</p>
+				</form>
 			</div>
 		</div>
 	)
