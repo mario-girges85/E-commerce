1c3a6d6ef7332c9b28b5412aa0272cd8dae18238
mario gerges <mario85.girges@gmail.com>
2024-10-02 16:08:07 +0200

    fixing bugs

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">
+        Responsive
+      </button>
+    </div>
+  );
 };
 
 export default Home;
