a618147aab2c9a8f2a369d5ac7ad3f0cdc6d4b3b
mario-girges85 <mario85.girges@gmail.com>
2024-10-17 07:10:39 -0700

    preparing editing the navbar to beshoy

--- src/components/navComponent/Nav.jsx
+++ src/components/navComponent/Nav.jsx
@@ -1,19 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import React, { useState } from "react";
 import ProfileMenuItems from "./ProfileMenuItems";
 import NavListItems from "./NavListItems";
 import { Bars3Icon } from "@heroicons/react/24/outline";
+import { Typography, Button } from "@material-tailwind/react";
 
-import DarkMood from "./DarkMood";
-import { Typography, Link, Button } from "@material-tailwind/react";
 const Nav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  console.log(localStorage.ud);
 
   return (
-    <div className=" items-center  flex-col flex-nowrap flex md:flex-row md:justify-between  content-center  px-6  py-1 shadow-md shadow-blue-gray-500 bg-white bg-opacity-90 justify-between  dark:bg-backcolor dark:bg-opacity-98 ">
+    <div className=" items-center z-50 relative py-4  flex-col flex-nowrap flex md:flex-row     px-6 shadow-md shadow-blue-gray-500 bg-white bg-opacity-90 justify-between  dark:bg-backcolor dark:bg-opacity-98 ">
+      {/* Menu Items - Hidden on mobile, displayed when the menu is toggled */}
+      <div
+        className={` ${
+          isMenuOpen ? "block" : "hidden"
+        } md:flex md:items-center  min-w-[fit]  `}
+      >
+        <NavListItems isMenuOpen={isMenuOpen} />
+      </div>
+
       {/* Logo is always displayed and burger */}
-      <div className="flex w-full  md:w-[10%] justify-between md:justify-around   items-center   ">
-        <h1 className="text-blue-gray-500 dark:text-maincolor font-semibold text-2xl">
+      <div className="flex justify-between  md:justify-around   items-center   ">
+        <h1 className="text-blue-gray-500  dark:text-white font-semibold text-2xl">
           Little Closet
         </h1>
         <div>
@@ -26,18 +35,19 @@ const Nav = () => {
         </div>
       </div>
 
-      {/* Menu Items - Hidden on mobile, displayed when the menu is toggled */}
-      <div
-        className={` ${
-          isMenuOpen ? "block" : "hidden"
-        } md:flex md:items-center  min-w-[fit]  `}
-      >
-        <NavListItems isMenuOpen={isMenuOpen} />
-      </div>
-
       {/* avatar */}
       <div>
-        <ProfileMenuItems />
+        {localStorage.ud == undefined ? (
+          // <Button>log in</Button>
+          <Link
+            className="text-2xl font-semibold dark:text-white dark:bg-maincolor bg-blue-gray-500 text-white rounded-lg p-2 m-2 "
+            to="/login"
+          >
+            Login
+          </Link>
+        ) : (
+          <ProfileMenuItems />
+        )}
       </div>
     </div>
   );
