961d615c169b5ebc68b1ee141bedabfd2ff9adb1
kyrolis <kerodex224@gmail.com>
2024-10-24 22:52:00 +0300

    add dark mode to login and sign up

--- src/pages/register_login_pages/SignUp.jsx
+++ src/pages/register_login_pages/SignUp.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import img1 from '../images/signup_img.svg'
+import img1 from '../images/img_3.json'
 import { Button, Checkbox, Input, Radio } from '@material-tailwind/react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import Lottie from 'lottie-react'
 
 const SignUp = () => {
 	const [user, setUser] = useState({
@@ -31,19 +32,18 @@ const SignUp = () => {
 		e.preventDefault()
 
 		const newErrors = {}
-
+		const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/
 		Object.keys(user).forEach((key) => {
 			if (user[key] === '') {
-				newErrors[key] = `${
-					key.charAt(0).toUpperCase() + key.slice(1)
-				} is required`
+				newErrors[key] = `${key.charAt(0).toUpperCase() + key.slice(1)} is required`
 			}
 		})
-
+		if (!emailRegex.test(user.email)) {
+			newErrors.email = 'not valid email'
+		}
 		if (user.password !== user.confirmPassword) {
 			newErrors.confirmPassword = "Passwords don't match"
 		}
-
 		if (Object.keys(newErrors).length > 0) {
 			setErrors(newErrors)
 			return
@@ -68,32 +68,27 @@ const SignUp = () => {
 	}
 
 	return (
-		<div className='flex w-full h-screen'>
-			<div className='hidden lg:flex h-full item-center justify-center'>
-				<img
-					src={img1}
-					alt='the Sign_up image'
-				/>
+		<div className='flex w-full h-screen dark:bg-[#050C9C]'>
+			<div className='hidden lg:flex ml-12 h-[full] item-center justify-center '>
+				<Lottie animationData={img1} />
 			</div>
-			<div className='w-full lg:w-2/3 flex items-center justify-center bg-gray-100'>
+			<div className='w-full lg:w-2/3 flex items-center justify-center bg-white dark:bg-[#050C9C]'>
 				<form
 					onSubmit={handleOnSubmit}
-					className='bg-white shadow-xl px-10 py-10 rounded-3xl border-2'
+					className='bg-white dark:bg-[#536493] shadow-xl px-10 py-10 rounded-3xl border-2'
 				>
-					<h1 className='flex justify-center text-3xl font-bold mb-10'>
+					<h1 className='flex dark:text-white justify-center text-3xl font-bold mb-10'>
 						Create New Account
 					</h1>
 
 					<div className='grid grid-cols-1 lg:grid-cols-2 justify-center w-full gap-6 lg:gap-9'>
 						<div>
 							{errors.firstName && (
-								<p className='text-red-500 font-bold mb-1 font-bold mb-1'>
-									{errors.firstName}
-								</p>
+								<p className='text-red-500 dark:text-white font-bold mb-1'>{errors.firstName}</p>
 							)}
 							<Input
+								className='dark:text-white '
 								name='firstName'
-								color='blue'
 								label='First Name'
 								error={Boolean(errors.firstName)}
 								value={user.firstName}
@@ -102,13 +97,11 @@ const SignUp = () => {
 						</div>
 						<div>
 							{errors.lastName && (
-								<p className='text-red-500 font-bold mb-1 '>
-									{errors.lastName}
-								</p>
+								<p className='text-red-500 dark:text-white   font-bold mb-1 '>{errors.lastName}</p>
 							)}
 							<Input
+								className='dark:text-white '
 								name='lastName'
-								color='blue'
 								label='Last Name'
 								error={Boolean(errors.lastName)}
 								value={user.lastName}
@@ -117,11 +110,11 @@ const SignUp = () => {
 						</div>
 						<div>
 							{errors.email && (
-								<p className='text-red-500 font-bold mb-1'>{errors.email}</p>
+								<p className='text-red-500 dark:text-white  font-bold mb-1'>{errors.email}</p>
 							)}
 							<Input
+								className='dark:text-white '
 								name='email'
-								color='blue'
 								label='Email'
 								error={Boolean(errors.email)}
 								value={user.email}
@@ -130,14 +123,13 @@ const SignUp = () => {
 						</div>
 						<div>
 							{errors.phonenumber && (
-								<p className='text-red-500 font-bold mb-1'>
-									{errors.phonenumber}
-								</p>
+								<p className='text-red-500 dark:text-white  font-bold mb-1'>{errors.phonenumber}</p>
 							)}
 							<Input
+								className='dark:text-white '
 								name='phonenumber'
-								color='blue'
-								label='phonenumber'
+								label='phone number'
+								type='number'
 								error={Boolean(errors.phonenumber)}
 								value={user.phonenumber}
 								onChange={updateUserInput}
@@ -145,11 +137,11 @@ const SignUp = () => {
 						</div>
 						<div>
 							{errors.password && (
-								<p className='text-red-500 font-bold mb-1'>{errors.password}</p>
+								<p className='text-red-500 dark:text-white  font-bold mb-1'>{errors.password}</p>
 							)}
 							<Input
+								className='dark:text-white '
 								name='password'
-								color='blue'
 								type={showPassword ? 'text' : 'password'}
 								label='Password'
 								error={Boolean(errors.password)}
@@ -159,13 +151,13 @@ const SignUp = () => {
 						</div>
 						<div>
 							{errors.confirmPassword && (
-								<p className='text-red-500 font-bold mb-1'>
+								<p className='text-red-500 dark:text-white  font-bold mb-1'>
 									{errors.confirmPassword}
 								</p>
 							)}
 							<Input
+								className='dark:text-white '
 								name='confirmPassword'
-								color='blue'
 								type={showPassword ? 'text' : 'password'}
 								label='Confirm Password'
 								error={Boolean(errors.confirmPassword)}
@@ -174,45 +166,41 @@ const SignUp = () => {
 							/>
 						</div>
 					</div>
-
-					<Checkbox
-						label='Show Password'
-						color='blue'
-						onChange={() => setShowPassword((prev) => !prev)}
-					/>
-
-					<div className='flex m-4'>
-						<p className='text-xl m-2'>
-							{user.gender === '' ? 'Please select one' : 'Gender'}
-						</p>
+					<div className=' flex items-center dark:text-white '>
+						<Checkbox onChange={() => setShowPassword((prev) => !prev)} />
+						Show Password'
+					</div>
+					<div className='flex items-center m-4 dark:text-white '>
+						{errors.gender ? (
+							<p className='text-red-500 dark:text-white  font-bold mb-1'>{errors.gender}</p>
+						) : (
+							'Gender'
+						)}
 						<Radio
 							name='gender'
-							label='Male'
 							value='male'
-							color='blue'
 							onChange={updateUserInput}
 						/>
+						male
 						<Radio
 							name='gender'
 							value='female'
-							label='Female'
-							color='blue'
 							onChange={updateUserInput}
 						/>
+						female
 					</div>
 
 					<Button
 						className='normal-case items-center text-lg text-center w-full'
-						color='blue'
 						type='submit'
 					>
 						Create Account
 					</Button>
 
-					<p className='flex items-center justify-center mt-5'>
+					<p className='flex dark:text-white  items-center justify-center mt-5'>
 						Already have an account?
 						<Link
-							className='text-blue-800 pl-1 hover:font-bold hover:-translate-y-0.5'
+							className='text-blue-800 dark:text-white   pl-1 hover:font-bold hover:-translate-y-0.5'
 							to='/login'
 						>
 							Log in
