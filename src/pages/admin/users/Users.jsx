34f3fe8c9bb9e27e12e1279e642f7b46123a6bcb
mario-girges85 <mario85.girges@gmail.com>
2024-10-28 15:19:45 -0700

    admin

--- src/pages/admin/users/Users.jsx
+++ src/pages/admin/users/Users.jsx
@@ -6,7 +6,7 @@ import User from "./User";
 
 const Users = ({ users, setUsers }) => {
   const [searchQuery, setSearchQuery] = useState("");
-//   const filteredUsers = users.filter((user) =>
+  //   const filteredUsers = users.filter((user) =>
   const [usersData, setUsersData] = useState(users);
 
   useEffect(() => {
@@ -15,9 +15,7 @@ const Users = ({ users, setUsers }) => {
     }
   }, [usersData]);
 
-
-    user?.firstName?.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  user?.firstName?.toLowerCase().includes(searchQuery.toLowerCase());
 
   const handleDeleteUser = (id) => {
     Swal.fire({
